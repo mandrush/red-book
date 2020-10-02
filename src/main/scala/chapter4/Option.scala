b44d34dec7f17626f57c979ad91fb26c2b5fab87
mandrush <andruszkiewiczm1@gmail.com>
2020-10-02 23:16:43 +0200

    better map2

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -6,27 +6,27 @@ sealed trait Option[+A] {
 
   def map[B](f: A => B): Option[B] = this match {
     case Some(a) => Some(f(a))
-    case None    => None
+    case None => None
   }
 
   def flatMap[B](f: A => Option[B]): Option[B] = this match {
     case Some(a) => f(a)
-    case None    => None
+    case None => None
   }
 
   def getOrElse[B >: A](default: => B): B = this match {
     case Some(a) => a
-    case None    => default
+    case None => default
   }
 
   def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
     case Some(_) => this
-    case None    => ob
+    case None => ob
   }
 
   def filter(f: A => Boolean): Option[A] = this match {
     case Some(a) if f(a) => this
-    case None            => None
+    case None => None
   }
 
   def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.length)
@@ -38,14 +38,23 @@ sealed trait Option[+A] {
   }
 
 }
+
 case class Some[+A](get: A) extends Option[A]
+
 case object None extends Option[Nothing]
 
 object OptionChapter extends App {
   def lift[A, B](f: A => B): Option[A] => Option[B] = x => x.map(f)
 
-  def Try[A](a: => A): Option[A] = try Some(a) catch { case _: Exception => None }
+  def Try[A](a: => A): Option[A] = try Some(a) catch {
+    case _: Exception => None
+  }
 
-  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-    a.flatMap(oa => b.map(ob => f(oa, ob)))
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    for {
+      oa <- a
+      ob <- b
+    } yield f(oa, ob)
+  }
+
 }
