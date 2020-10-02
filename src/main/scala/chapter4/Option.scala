a36f2bf65ccacdf6397ad0772afd4b2d7c490a72
mandrush <andruszkiewiczm1@gmail.com>
2020-10-02 23:15:27 +0200

    42 43

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -29,6 +29,23 @@ sealed trait Option[+A] {
     case None            => None
   }
 
+  def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs).flatMap { m =>
+      mean(xs.map(x => math.pow(x - m, 2)))
+    }
+  }
+
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
+
+object OptionChapter extends App {
+  def lift[A, B](f: A => B): Option[A] => Option[B] = x => x.map(f)
+
+  def Try[A](a: => A): Option[A] = try Some(a) catch { case _: Exception => None }
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(oa => b.map(ob => f(oa, ob)))
+}
